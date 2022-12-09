9c1280de4df4e29024853a1945ad9cf69c7af84b
Vladimir Zakharov <netsla@yandex.ru>
2022-12-09 18:14:47 +0300

    initial python

---
+++ 3_task.py
@@ -0,0 +1,3 @@
+first_animal = input('Введите первое животное (Заяц): ')
+second_animal = input('Введите второе животное (Черепаха): ')
+print(first_animal + ' спит,' + second_animal + ' идёт')
---
+++ 5_task.py
@@ -0,0 +1,4 @@
+user = input('Введите Ваше ФИО: ')
+city_departure = input('Введите город отправления: ')
+city_arrival = input('Введите город прибытия: ')
+print('Уважаемый ' + user + '!' + ' Вы заказали билет с городом отправления: ' + city_departure + ' и городом прибытия: ' + city_arrival)
